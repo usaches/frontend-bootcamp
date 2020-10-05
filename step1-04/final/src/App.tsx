916fb55a60510cebfdec23dcc3c22f85ad8b3927
usaches <38760409+usaches@users.noreply.github.com>
2020-10-05 15:46:20 -0400

    Update App.tsx

--- step1-04/final/src/App.tsx
+++ step1-04/final/src/App.tsx
@@ -9,6 +9,22 @@ export class App extends React.Component<any, any> {
         <Counter text="Chickens" />
         <Counter text="Ducks" />
       </div>
+      
+      
+      
+       <div className="12">
+        <h2>react</h2>
+        <Counter text="panda" />
+        <Counter text="pig" />
+      </div>
+      
+      
+      
+       <div className="Blank">
+        <h212</h2>
+        <Counter text="45" />
+        <Counter text="212" />
+      </div>
     );
   }
 }
